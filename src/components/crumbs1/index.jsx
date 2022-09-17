e4c493be85326fa9f1735790b06270c0cda443d1
liu guoliang <liuguoliang@hanhou.cn>
2022-09-17 20:24:54 +0800

    1

--- src/components/crumbs1/index.jsx
+++ src/components/crumbs1/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import './index.scss'
+import utils from '../../utils/index'
 const backIMG = require('../../assets/back.png')
 function Crumbs1 (props) {
   const navigate = useNavigate()
@@ -11,12 +12,23 @@ function Crumbs1 (props) {
       navigate(-1)
     }
   }
+  const BackBtnRender = () => {
+    let com = null
+    utils.ownApp(() => {
+
+    }, () => {
+      com = (
+        <img src={backIMG} alt="" className="back" onClick={() => onBack()} />
+      )
+    })
+    return com
+  }
   return (
     <>
     <div className="crumbs1-slot"></div>
     <div className="crumbs1">
       <div className="content">
-        <img src={backIMG} alt="" className="back" onClick={() => onBack()} />
+        <BackBtnRender></BackBtnRender>
         <span>{props.text}</span>
       </div>
     </div>
