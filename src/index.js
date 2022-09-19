31c59410ce56d9e402bc4fa3edc8fbb88efc560b
liu guoliang <liuguoliang@hanhou.cn>
2022-09-19 23:08:24 +0800

    test

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import utils from './utils/index'
 import { Toast } from 'antd-mobile'
 // document.cookie = 'x-token=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJVVUlEIjoiZWM1MmQ5NDUtZmJiNS00MTk2LThkZWQtYWVhMDRiNGJlYjEzIiwiSUQiOjEwMDAzMiwiUGhvbmUiOiIxNTgwMTI1MjM5MiIsIkJ1ZmZlclRpbWUiOjg2NDAwLCJleHAiOjE2NjM5MDQ0ODcsImlzcyI6IkhBSEFPbzYzMjQ2MzEiLCJuYmYiOjE2NjMyOTg2ODd9.k8JSInMzNt2lhD2AXPwEb_9yb9RyBHdDfRuO272zxsxa20R3o7kKhdPFOH6vjsCDqN6B2BrYI8QLzrMRFiBUMA'
 utils.ownApp(() => {
+  alert('document.cookie：', document.cookie)
   if (document.cookie) {
     console.log(document.cookie)
     const arr = document.cookie.split('&')
