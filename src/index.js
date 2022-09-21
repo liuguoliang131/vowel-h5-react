017cf8b42d21910b4e95e576262e7df4d1ad2864
liu guoliang <liuguoliang@hanhou.cn>
2022-09-21 15:26:07 +0800

    ios

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.js'
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
+// ios穿透问题
+document.getElementsByName('html')[0].addEventListener('touchmove', function (e) {
+  e.preventDefault()
+})
+const rootDOM = document.getElementById('root')
+const root = ReactDOM.createRoot(rootDOM)
 root.render(
   // <React.StrictMode>  严格模式会让react组件触发两次
   <App />
