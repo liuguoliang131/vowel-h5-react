611800a182c2ca79de4095b25b3bfdb1e44441db
liu guoliang <liuguoliang@hanhou.cn>
2022-09-21 16:54:07 +0800

    显示audio

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -15,7 +15,7 @@ import Dialog from '../../components/dialog/index.jsx'
 import FirstPop from './components/firstPop'
 import utils from '../../utils'
 import { promotionActivityDetailApi } from '../../axios/api'
-import { mockRequest } from './mock-request'
+// import { mockRequest } from './mock-request'
 let timer = null
 const Home = (props) => {
   // const saveCallBack = useRef()
@@ -104,12 +104,12 @@ const Home = (props) => {
         }
       }
 
-      // const res = await promotionActivityDetailApi(params)
-      // if (res.code !== 0) {
-      //   setEmptyMsg(res.msg)
-      //   return false
-      // }
-      const res = await mockRequest()
+      const res = await promotionActivityDetailApi(params)
+      if (res.code !== 0) {
+        setEmptyMsg(res.msg)
+        return false
+      }
+      // const res = await mockRequest()
       console.log('res', res)
       res.data.draw_end_time *= 1000
       res.data.draw_start_time *= 1000
