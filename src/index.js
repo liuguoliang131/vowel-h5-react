c10c5ec2a5c18a6852e46e88c55414130e18de92
liu guoliang <liuguoliang@hanhou.cn>
2022-09-22 15:10:44 +0800

    meta

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.js'
-
+document.addEventListener('gesturestart', function (event) {
+  event.preventDefault()
+})
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   // <React.StrictMode>  严格模式会让react组件触发两次
